07ba245c80416a58d6de771951236e1b3e16e872
Harsh Dana <harshdana1@gmail.com>
2024-06-20 15:43:49 +0530

    fix...

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ import GitHubRepo from "@/components/GithubRepos";
 export default function Home() {
   return (
     <main className="p-8 md:p-10 md:pt-20 text-gray-100">
-      {/* Main title decor */}
+    
       <h1 className="decor absolute opacity-20 -mt-14 ml-50 pl-16 hidden md:inline-block">
-        ///
+        {"///"}
       </h1>
       <div className="relative flex flex-col md:flex-row items-center justify-evenly md:gap-10">
         <Profile />
@@ -19,7 +19,6 @@ export default function Home() {
         <AboutSection />
       </section>
       <section className="relative">
-        {/* Skills title decor */}
         <h1 className="decor absolute opacity-20 right-0 mt-40 hidden md:inline-block">
           SKills
         </h1>
@@ -29,7 +28,6 @@ export default function Home() {
       </section>
 
       <section>
-        {/* Projects title decor */}
         <h1 className="decor absolute opacity-20 -mt-14 ml-64 pl-16 hidden md:inline-block">
           Projects
         </h1>
